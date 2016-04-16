30ca91646bd18ebb2e02916e9a87a961ed6d81a9
Jose Luiz Tafarel <jltafarel@gmail.com>
2016-04-16 14:14:25 -0300

    Create splash screen

--- Hackarejo/app/src/main/java/io/github/hackarejo/equipe9/MainActivity.java
+++ Hackarejo/app/src/main/java/io/github/hackarejo/equipe9/MainActivity.java
@@ -1,13 +1,30 @@
 package io.github.hackarejo.equipe9;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
     }
 }
